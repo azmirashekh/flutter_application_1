f46491e9b222c5c6423245bc240c55f7b23d1178
azmirashekh <shekh15-5569@diu.edu.bd>
2025-02-18 13:00:00 +0600

    Image DESIGN

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const LabClass05());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+/* Overflow Handling
+1. SingleChildScrollView
+2. Expanded
+3. Flexible
+*/
+
+class LabClass05 extends StatelessWidget {
+  const LabClass05({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Basic UI - 02",
+      title: "TheFirstApp",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Flutter Basic UI - 02"),
-          backgroundColor: Colors.blue,
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
+          title: Text("TheFirstApp"),
+          
         ),
-        body: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Container(
-              //height: 600,
-               width: 600,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: List.generate(
-                  100,
-                  (index) => Icon(
-                    Icons.star,
-                    color: const Color.fromARGB(255, 227, 19, 19),
-                    size: index.toDouble(),
+        body:Container(
+          margin: EdgeInsets.all(20),
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("50%off",
+                  style: TextStyle(fontSize: 20),
                   ),
-                ),
+                  Icon(Icons.heart_broken,size: 30,)
+
+                ],
+                
               ),
-            ),
+             Image(image: AssetImage("assets\images\1217812-800-800.webp")),
+             Text(" Shoes")
+             ,
+             Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("\$450",
+                style: TextStyle(
+                  fontSize: 30
+                ),),
+                Text("\$500",style: TextStyle(
+                  fontSize: 20
+                ),)
+
+              ],
+             )
+            ],
           ),
-        ));
+        ) 
+      ),
+    );
   }
 }
 
