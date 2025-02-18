e940df226f5ea7591e41800b02ba7758f9d9289b
azmirashekh <shekh15-5569@diu.edu.bd>
2025-02-18 10:12:08 +0600

    init q

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Test());
+  runApp(const LabClass05());
 }
 // Based on Layout
 /*
@@ -12,48 +12,52 @@ void main() {
 assets\images\photo01.png
 */
 
-class Test extends StatelessWidget {
-  const Test({super.key});
+class LabClass05 extends StatelessWidget {
+  const LabClass05({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic UI - Flutter",
+      title: "Basic UI - Flutter 02",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Basic UI - Flutter"),
+          title: Text("Basic UI - Flutter 02"),
+          backgroundColor: Colors.blue[400],
           centerTitle: true,
           elevation: 3.5,
           foregroundColor: Colors.blue,
           shadowColor: const Color.fromARGB(167, 128, 128, 128),
         ),
-        body: Container(
-          margin: EdgeInsets.all(50),
-          padding: EdgeInsets.all(30),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/photo01.png'),
-              fit: BoxFit.cover,
-              alignment: Alignment.center,
-              opacity: 1,
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Icon(
+              Icons.ac_unit,
+              size: 50,
+              color: Colors.blueGrey,
             ),
-            boxShadow: [
-              BoxShadow(
-                color: const Color.fromARGB(154, 255, 247, 0),
-                blurRadius: 20,
-                blurStyle: BlurStyle.outer,
+            Text(
+              "Hi",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 30,
               ),
-              BoxShadow(
-                color: const Color.fromARGB(255, 34, 34, 34),
-                blurRadius: 10,
-                blurStyle: BlurStyle.outer,
+            ),
+            Text(
+              "This is",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
               ),
-            ],
-            color: const Color.fromARGB(255, 247, 255, 7),
-            borderRadius: BorderRadius.circular(50),
-          ),
-          child: Text("Welcome Back..!"),
+            ),
+            Text(
+              "Flutter",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 30,
+              ),
+            ),
+          ],
         ),
       ),
     );
